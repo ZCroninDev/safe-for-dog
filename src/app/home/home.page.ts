24a99f381927d3b21585a67848b1b5d382684b94
Zach <zcronin1@gmail.com>
2022-05-13 17:23:38 -0400

    display json data in html

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as foodList from 'src/assets/foodList.json'
+
 @Component({
 	selector: 'app-home',
 	templateUrl: 'home.page.html',
@@ -8,22 +10,27 @@ import { Component } from '@angular/core';
 export class HomePage {
 	search: string;
 	validSearch: boolean = false;
+	foods: object;
+	foodData: object;
 
 	constructor() {
-		
+		// console.log(typeof(foodList));
+		this.foods = JSON.parse(JSON.stringify(foodList));
+		console.log(foodList)
+		console.log(this.foods['banana'])
 	}
 
-	userSearch(str: string) {
-		console.log('logging search', str)
-		if (str === 'bananas') {
+	
+	userSearch(foodBeingSearched: string) {
+		
+		if (this.foods[foodBeingSearched]) {
 			console.log('Valid search')
 			this.validSearch = true;
+			this.foodData = this.foods[foodBeingSearched];
 		} else {
 			console.log('Invalid search')
-			this.validSearch = false;
-			
+			this.foodData = {};
 		}
 	}
-
 	
 }
