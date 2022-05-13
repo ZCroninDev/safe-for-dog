a2cc649a5e190c020cfc469c3d547d2b40861222
Zach <zcronin1@gmail.com>
2022-05-13 12:28:36 -0400

    Search function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	search: string;
+	validSearch: boolean = false;
 
-  constructor() {}
+	constructor() {
+	}
 
+	userSearch(str: string) {
+		console.log('logging search', str)
+		if (str === 'bananas') {
+			console.log('Valid search')
+			this.validSearch = true;
+		} else {
+			console.log('Invalid search')
+			this.validSearch = false;
+			
+		}
+	}
+	
 }
